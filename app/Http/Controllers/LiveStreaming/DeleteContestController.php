80d843d9b58fde17965f6ecd737681063439fe3d
h4r1yzz <90262202+h4r1yzz@users.noreply.github.com>
2023-12-04 15:54:26 +0800

    DeleteContestController.php

--- app/Http/Controllers/LiveStreaming/DeleteContestController.php
+++ app/Http/Controllers/LiveStreaming/DeleteContestController.php
@@ -5,18 +5,36 @@ namespace App\Http\Controllers\LiveStreaming;
 use Illuminate\Http\Request;
 use App\Models\Events;
 
+/**
+ * Class DeleteContestController
+ *
+ * This controller is responsible for handling the deletion of contests.
+ *
+ * @package App\Http\Controllers\LiveStreaming
+ */
+
 class DeleteContestController
 {
+    /**
+     * Deletes a contest based on the provided ID.
+     *
+     * @param int $id The ID of the contest to be deleted.
+     * @return \Illuminate\Http\RedirectResponse Redirects back with success or error message.
+     */
     public function delete($id)
     {
+        // Find the contest by ID
         $event = Events::find($id);
 
+        // Check if the contest exists
         if (!$event) {
-            return redirect()->route('contests.post')->with('error', 'Contest not found');
+            return back()->with('error', 'Contest not found');
         }
 
+        // Delete the contest
         $event->delete();
 
-        return redirect()->route('contests.post')->with('success', 'Contest deleted successfully');
+        // Redirect back with success message
+        return back()->with('success', 'Contest deleted successfully');
     }
 }
