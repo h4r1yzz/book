86f2323a218fab4955ec810400db142e0ca5b7e8
h4r1yzz <90262202+h4r1yzz@users.noreply.github.com>
2023-12-04 15:57:47 +0800

    StoreContestRequest.php

--- app/Http/Requests/StoreContestRequest.php
+++ app/Http/Requests/StoreContestRequest.php
@@ -22,32 +22,26 @@ class StoreContestRequest extends FormRequest
     public function rules(): array
     {
         return [
-
             'title' => 'required|string|max:255',
             'contest_start_at' => 'required',
-            'contest_end_at' => 'required|date|after_or_equal:contest_start_at', 
-
+            'contest_end_at' => 'required|date|after_or_equal:contest_start_at',
             'contest_display_start_at' => 'required',
             'contest_display_end_at' => 'required|date|after_or_equal:contest_start_at',
             'contest_type' => 'required|in:single,multi',
-
             'gifts_bounded' => 'nullable|array',
-            'gifts_bounded.*.id' => 'nullable',
-            'gifts_bounded.*.pricing_id' => 'nullable',
-            
+            'gifts_bounded.*.id' => 'sometimes',
+            'gifts_bounded.*.pricing_id' => 'sometimes',
             'graphics' => 'array',
             'graphics.*.type' => 'nullable',
             'graphics.*.asset_url' => 'nullable|file|max:100000',
             'graphics.*.reference' => 'nullable',
             'graphics.*.url' => 'nullable',
-
             'graphics.*.text' => 'required',
-            'graphics.*.is_countdown_required' => 'boolean',
+            'graphics.*.is_countdown_required' => 'sometimes|boolean',
             'graphics.*.title_font_color' => 'required|regex:/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/',
-            'graphics.*.countdown_font_color' => 'required|regex:/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$',
+            'graphics.*.countdown_font_color' => 'required|regex:/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/',
             'graphics.*.template' => 'required',
-
-
+            'tier_system' => 'required|integer',
         ];
     }
 }
