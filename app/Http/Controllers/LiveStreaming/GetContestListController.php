6999607ff6c9b777c3ffbca2ef2752b45da20261
h4r1yzz <90262202+h4r1yzz@users.noreply.github.com>
2023-12-04 15:56:13 +0800

    GetContestListController.php

--- app/Http/Controllers/LiveStreaming/GetContestListController.php
+++ app/Http/Controllers/LiveStreaming/GetContestListController.php
@@ -4,27 +4,27 @@ namespace App\Http\Controllers\LiveStreaming;
 use Illuminate\Http\Request;
 use App\Models\Events;
 
+/**
+ * Class GetContestListController
+ *
+ * This controller is responsible for retrieving and displaying a list of contests.
+ *
+ * @package App\Http\Controllers\LiveStreaming
+ */
 class GetContestListController
-
-{
-    public function index(Request $request) 
-    {
-        
-        $events = Events::get();
-
-        foreach ($events as $event)
-        {
-            dd($event);
-        }
-        dd($events->count());
-    }
-     
+{    
+    /**
+     * Displays a paginated list of contests ordered by start date.
+     *
+     * @param \Illuminate\Http\Request $request The HTTP request instance.
+     * @return \Illuminate\View\View Returns the contests view with paginated contest data.
+     */
     public function display(Request $request)
     {
-        $events = Events::all();
-        
-        //dd($contests);
+        // Retrieve contests, order them by start date in descending order, and paginate
+        $events = Events::orderBy('contest_start_at', 'desc')->paginate(10);
 
+        // Return the contests view with the paginated contest data
         return view('contests.post', compact('events'));
     }
 
